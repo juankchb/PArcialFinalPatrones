4db9d5406dda1cc133043e9d08dd2f2163122cb2
juankchb <89666771+juankchb@users.noreply.github.com>
2023-06-02 11:20:20 -0500

    Update AvionG.java

--- src/ModeloVista/AvionG.java
+++ src/ModeloVista/AvionG.java
@@ -2,17 +2,45 @@ package ModeloVista;
 
 import Modelo.Coordenadas;
 
-public abstract class AvionG {
-
+public abstract class AvionG implements Observable{
+	private List<Observer> observers;
+        private int id;
+        private boolean llegoADestino;
 	protected Coordenadas cords;
 
-	public AvionG(Coordenadas cords) {
-		super();
-		this.cords = cords;
-	}
-
+	public AvionG( int id,Coordenadas cords) {
+        super();
+	this.id = id;
+        this.llegoADestino = false;
+	this.cords = cords;
+        this.observers = new ArrayList<>();
+    }
 	public Coordenadas getCords() {
 		return cords;
 	}
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
 
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+	public void llegoADestino() {
+        this.llegoADestino = true;
+        notifyObservers();
+    }
+
+    public boolean haLlegadoADestino() {
+        return llegoADestino;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
